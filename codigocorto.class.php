de42dc64a03272d5a08beaf93915b0ba9947c3e8
francisco1712 <59690182+francisco1712@users.noreply.github.com>
2022-03-07 21:50:47 +0100

    Add files via upload

--- codigocorto.class.php
+++ codigocorto.class.php
@@ -60,7 +60,20 @@
                 
                 ";
             }elseif ($tipo == 2) {
-                
+                $html="
+                <diV class='from-group'>
+                    <p><b>$pregunta</b></p>
+                  <div class='col-sm-8'>  
+                        <select class='from-control' id='$detalleid' name='$detalleid'>
+                                <option value='0'>0</option>
+                                <option value='1'>1</option>
+                                <option value='2'>2</option>
+                                <option value='3'>3</option>
+                                <option value='4'>4</option>
+                                <option value='5'>5</option>
+                        </select>
+                  </div>
+            ";
             }else{
     
             }
